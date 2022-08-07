320862e28057e90190d6c1091b25d292bc3c52d3
gooxpf <gooxpf@gmail.com>
2022-08-07 11:55:45 +0400

    [feat] install sass, add style structure.

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,28 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.tsx</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>H1 example</h1>
+      <h2>H2 example</h2>
+      <h3>H3 example</h3>
+      <h4>H4 example</h4>
+      <h5>H5 example</h5>
+      <h6>H6 example</h6>
+      <p>
+        Example p tag, lorem ispum
+        <span> Test Span</span>
+      </p>
+      <div>Application</div>
+      <button type="button">TEST BUTTON</button>
+      <button type="button">TEST BUTTON LONG TEXT</button>
     </div>
   );
 }
